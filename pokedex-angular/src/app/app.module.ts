18260afde78b003dd49962c89abf8e8d0e1308e3
Kate <ekaterina.kuschenko@gmail.com>
2020-03-23 22:53:51 +0300

    Import the pokedex.module.ts file inside the app.module.ts file.

--- pokedex-angular/src/app/app.module.ts
+++ pokedex-angular/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PokedexModule } from './pokedex/pokedex.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PokedexModule],
   providers: [],
   bootstrap: [AppComponent]
 })
